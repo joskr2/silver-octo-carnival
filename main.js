41dc40a404dc9e7e380c03e4d34acad929741e95
Josue Patricio Retamozo Vargas <josretamozovar@gmail.com>
2023-05-18 22:22:10 -0500

    transformacion class component

--- main.js
+++ main.js
@@ -1,136 +1,118 @@
-const MiCalculadora = () => {
+class MyCalculator extends HTMLElement {
+  constructor() {
+    super();
+    this._shadowRoot = this.attachShadow( { mode: 'open' } );
+    this._currentNumber = '0';
+    this._operator = null;
+    this._operand = null;
+  }
 
-  let numeroActual = '0';
-  let operador = null;
-  let operando = null;
-  let mensajeError = "";
+  connectedCallback() {
+    this.render();
+  }
 
-  const calcularResultado = () => {
-    switch ( operador ) {
-      case '+':
-        numeroActual = ( operando + parseFloat( numeroActual ).toFixed( 2 ) ).toString();
-        break;
-      case '-':
-        numeroActual = ( operando - parseFloat( numeroActual ).toFixed( 2 ).toString() );
-        break;
-      case '*':
-        numeroActual = ( operando * parseFloat( numeroActual ).toFixed( 2 ).toString() );
-        break;
-      case '/':
-        if ( numeroActual === '0' ) {
-          mensajeError = "Error: No se puede hacer divisiones por cero"
-        }
-        numeroActual = ( operando / parseFloat( numeroActual ).toFixed( 2 ).toString() );
-        break;
-      default:
-        break;
+  render() {
+    // Creación del HTML
+    this._shadowRoot.innerHTML = `
+    <style>
+    #calculadora {
+      @apply bg-blue-500 rounded-lg p-4
     }
-    operador = null;
-    operando = null;
-  };
-
-  const manejarClick = ( valorBoton ) => {
-
-    mensajeError = "";
-
-    switch ( valorBoton ) {
-      case '+':
-      case '-':
-      case '*':
-      case '/':
-
-        // if ( operador !== null ) {
-        //   calcularResultado();
-        // }
-
-        operador !== null && calcularResultado();
-
-        operador = valorBoton;
-        operando = parseFloat( numeroActual );
-        numeroActual = '0';
-        break;
-      case '=':
-        if ( operador === null || operando === null ) {
-          mensajeError = "Error: Operador u operando no definidos";
-          return;
-        }
-        calcularResultado();
-        break;
-      case 'C':
-        numeroActual = '0';
-        operador = null;
-        operando = null;
-        break;
-      default:
-        // va a permitir que solo ingrese numeros , y que estos solo tengan un punto decimal
-        if ( !/^(\\d+\\.?\\d*|\\.\\d+)$/.test( numeroActual + valorBoton ) ) {
-          mensajeError = "Error: Entrada invalida";
-          return;
-        }
-
-        numeroActual += valorBoton;
-        break;
+    #pantalla {
+      @apply bg-white text-right py-2 px-4 mb-2 rounded
+    } 
+    button {
+      @apply bg-blue-200 py-2 px-4 rounded-full m-2
     }
-    renderizar();
-  };
+    #mensajeError {
+      @apply  bg-white text-red-800 text-right py-2 px-4 mb-2 rounded
+    }
 
-  const renderizar = () => {
-    const shadowRoot = this.attachShadow( { mode: 'open' } );
-    shadowRoot.innerHTML = `
-      <style>
-        #calculadora {
-          @apply bg-blue-500 rounded-lg p-4
-        }
-        #pantalla {
-          @apply bg-white text-right py-2 px-4 mb-2 rounded
-        } 
-        button {
-          @apply bg-blue-200 py-2 px-4 rounded-full m-2
-        }
-        #mensajeError {
-          @apply  bg-white text-red-800 text-right py-2 px-4 mb-2 rounded
-        }
-      </style>
+    
+  </style>
 
-      <div id="calculadora">
-        <div id="pantalla">
-          ${numeroActual}
-        </div>
+  <div id="calculadora">
+    <div id="pantalla">
+      ${this._currentNumber}
+    </div>
 
-      <button>
-      1
-      </button>
-      <button>
-      2
-      </button>
-      <button>
-      3
-      </button>
-      <button>
-      +
-      </button>
-      <button>
-      -
-      </button>
-      <button>
-      =
-      </button>
+  <button>
+  1
+  </button>
+  <button>
+  2
+  </button>
+  <button>
+  3
+  </button>
+  <button>
+  +
+  </button>
+  <button>
+  -
+  </button>
+  <button>
+  =
+  </button>
 
-        <div id="mensajeError">
-        ${mensajeError}
-        </div>
-      </div>
+  </div>
     `;
 
-    shadowRoot.querySelectorAll( "button" ).forEach( ( boton ) => {
-      boton.addEventListener( 'click', ( evento ) => {
-        const valorBoton = evento.target.innerText;
-        manejarClick( valorBoton );
-      } )
-    } )
-  };
+    // Agregar eventos a los botones
+    this._shadowRoot.querySelectorAll( 'button' ).forEach( ( button ) => {
+      button.addEventListener( 'click', ( event ) => {
+        const buttonValue = event.target.innerText;
 
-  renderizar();
-};
+        switch ( buttonValue ) {
+          case '+':
+          case '-':
+          case '*':
+          case '/':
+            this._operator = buttonValue;
+            this._operand = parseFloat( this._currentNumber );
+            this._currentNumber = '0';
+            break;
+          case '=':
+            this.calculateResult();
+            break;
+          case 'C':
+            this._currentNumber = '0';
+            this._operator = null;
+            this._operand = null;
+            break;
+          default:
+            this._currentNumber += buttonValue;
+            break;
+        }
 
-customElements.define( "mi-calculadora", MiCalculadora);
+        this.render();
+      } );
+    } );
+  }
+
+  calculateResult() {
+    switch ( this._operator ) {
+      case '+':
+        this._currentNumber = this._operand + parseFloat( this._currentNumber );
+        break;
+      case '-':
+        this._currentNumber = this._operand - parseFloat( this._currentNumber );
+        break;
+      case '*':
+        this._currentNumber = this._operand * parseFloat( this._currentNumber );
+        break;
+      case '/':
+        this._currentNumber = this._operand / parseFloat( this._currentNumber );
+        break;
+      default:
+        break;
+    }
+
+    this._currentNumber = this._currentNumber.toString();
+    this._operator = null;
+    this._operand = null;
+  }
+
+}
+
+customElements.define( 'my-calculator', MyCalculator );
